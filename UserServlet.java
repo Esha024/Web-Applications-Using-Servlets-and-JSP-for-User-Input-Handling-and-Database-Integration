fb8184f850a1f50af4bb72a6e25c134d938f9c16
Esha024 <Chauhanesha90@gmail.com>
2025-11-04 11:49:02 +0530

    Create UserServlet.java

---
+++ UserServlet.java
@@ -0,0 +1,37 @@
+package com.example.webapp;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+public class UserServlet extends HttpServlet {
+    private UserDAO userDAO;
+
+    @Override
+    public void init() {
+        userDAO = new UserDAO();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        User user = new User(name, email, password);
+
+        try {
+            int result = userDAO.registerUser(user);
+            if (result > 0) {
+                response.sendRedirect("success.jsp");
+            } else {
+                response.sendRedirect("error.jsp");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.sendRedirect("error.jsp");
+        }
+    }
+}
